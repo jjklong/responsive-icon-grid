919af44e5a751588ce57be4ef7a373405bc0ab09
Jordan Long <jordan.long222@gmail.com>
2016-06-15 18:56:45 -0400

    add jquery hover background color change

--- templates/src/assets/js/grid.js
+++ templates/src/assets/js/grid.js
@@ -1,1 +1,27 @@
-console.log('hey');
+$(function(){
+   'use strict';
+//1) When hovering over list item (.iconList):
+//      - background-color changes
+//      - bottom padding?margin?height changes
+//      - text areas switch spots
+//      - new text category displays
+//2) easily replace icons and text updates
+//3) add input to allow user to pick icon
+//
+//
+//
+var color = 'lightblue';
+var hover = 'white';
+
+$('.iconList').hover( function onhover() {
+  $(this).css('background-color', color);
+}, function offhover() {
+  $(this).css('background-color', hover);
+});
+
+//display icon name
+// $('.h3title')
+
+
+
+}); //use strict
